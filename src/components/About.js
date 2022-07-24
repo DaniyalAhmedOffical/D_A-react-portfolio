fbdcd70fdb35f942dc1fd87d7b38706509df010f
daniyalahmed <daniyalaq111@gmail.com>
2022-07-24 17:54:16 +0500

    react portfolio

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+        Hi, I'm Daniyal Ahmed and I am Front End developer & Designer with industry experience building websites and web applications along with the 2-year experience in Web-developing
+        Specializer's in Web Applications Development and I have professional experience working with different, frameworks & CMS.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+        My Skillset contains GRAPHIC DESIGNING, FRONT-END DEVELOPMENT , HTML5 CSS3, SCSS, Bootstrap, React Js, React Native, React Native, React Bootstrap, Redux WordPress, Custom JavaScript Language.
+        I always obtain a challenging position as a developer that will enable me to utilize my experience and strong organizational skills, interpersonal skills, and ability to meet the goals of the company.
+         </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,264 @@
+// import React from "react";
+// import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+// import installNode from "../assets/portfolio/installNode.jpg";
+// import navbar from "../assets/portfolio/navbar.jpg";
+// import reactParallax from "../assets/portfolio/reactParallax.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
+// const Portfolio = () => {
+//   const portfolios = [
+//     {
+//       id: 1,
+//       src: arrayDestruct,
+//       href: "https://github.com/DaniyalAhmedOffical",
+//     },
+//     {
+//       id: 2,
+//       src: reactParallax,
+//     },
+//     {
+//       id: 3,
+//       src: navbar,
+//     },
+//     {
+//       id: 4,
+//       src: reactSmooth,
+//     },
+//     {
+//       id: 5,
+//       src: installNode,
+//     },
+//     {
+//       id: 6,
+//       src: reactWeather,
+//     },
+//   ];
+
+//   return (
+//     <div
+//       name="portfolio"
+//       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+//     >
+//       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+//         <div className="pb-8">
+//           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+//             Portfolio
+//           </p>
+//           <p className="py-6">Check out some of my work right here</p>
+//         </div>
+
+//         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+//           {portfolios.map(({ id, src }) => (
+//             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+//               <img
+//                 src={src}
+//                 alt=""
+//                 className="rounded-md duration-200 hover:scale-105"
+//               />
+//               <div className="flex items-center justify-center">
+//                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+//                   Demo
+//                 </button>
+//                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+//                   Code
+//                 </button>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import store from '../assets/store1.jpg'
+import expense from '../assets/expense-tracking-app.jpg'
+import baking from '../assets/baking.jpg'
+import paper from '../assets/paper.jpg'
+import pop from '../assets/pop.jpg'
+import clock from '../assets/clock.jpg';
+const Portfolio = () => {
+  return (
+  
+    <div name='work' className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-sky-600'>
+            Work
+          </p>
+          <p className='py-6'> Check out some of my recent work</p>
+        </div>
+
+        {/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${expense})`, height: "250px", width: "309px" }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+                React Expense App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://react-redux-expense-tracker-app-by-daniyal-ahmed-official.netlify.app'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/DaniyalAhmedOffical/react-quiz-app-by-D_A-PRODUCTIONS'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${paper})`, height: "250px", width: "309px" }}
+
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+                React Quiz App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://react-quiz-app-by-daniyal-ahmed-official.netlify.app'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/DaniyalAhmedOffical/react-quiz-app-by-D_A-PRODUCTIONS'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${pop})`, height: "250px", width: "309px" }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                POP The Ball Game
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://daniyalahmedoffical.github.io/pop-the-ball-game/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/DaniyalAhmedOffical/pop-the-ball-game'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${baking})`, height: '250px', width: '300px' }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                D_A Food Website
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${store})`, height: "250px", width: "309px" }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                D_A Fashion Website
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://daniyalahmedoffical.github.io/DAFashions/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/DaniyalAhmedOffical/DAFashions'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${clock})`, height: "250px", width: "309px" }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Digital Clock Web App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href=' https://daniyalahmedoffical.github.io/D_A-Digital_Clock/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/DaniyalAhmedOffical/D_A-Digital_Clock'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
